75fe348e37677f155f47a222896067363231730e
Vitali <suom1999@mail.ru>
2019-09-06 15:40:04 +0300

    WriteLine Change

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Added");
+            Console.WriteLine("Change");
         }
     }
 }
